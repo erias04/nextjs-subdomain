1b4db1a035f6d9850495cdb292532bd4375103c1
erias04 <github.eliascerne@icloud.com>
2022-03-01 10:26:17 +0100

    delted team_id in api Because it is not deployed within a tem in vercel

--- pages/api/add-domain.js
+++ pages/api/add-domain.js
@@ -4,7 +4,7 @@ export default async function addDomain(req, res) {
   const { domain, siteId } = req.query;
 
   const response = await fetch(
-    `https://api.vercel.com/v8/projects/${process.env.VERCEL_PROJECT_ID}/domains?teamId=${process.env.VERCEL_TEAM_ID}`,
+    `https://api.vercel.com/v8/projects/${process.env.VERCEL_PROJECT_ID}/domains`,
     {
       body: `{\n  "name": "${domain}"\n}`,
       headers: {
